55933244ca1fdd81e6102ba5d7651942f11534d6
Palak Garg <palakgarg029@gmail.com>
2026-09-13 20:27:18 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+
+        unordered_set<int> s;
+
+        for(int x : nums) {
+
+            if(s.find(x) != s.end()) {
+                return true;
+            }
+
+            s.insert(x);
+        }
+
+        return false;
+    }
+};
